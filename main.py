27cb83872733521fbbc4092cff2e06dd6d727fb5
lifunext <ad@lifunext.com>
2025-05-20 21:50:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,3 +38,7 @@ def fetch():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
